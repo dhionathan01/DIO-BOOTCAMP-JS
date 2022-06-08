c80612512360d606fb905e79ce5d8c8ce8f933bf
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-06-08 15:31:14 -0300

    Introdução - Var e Const

---
+++ Codigos/Recursos_Basicos_da_Linguagem/teste.js
@@ -0,0 +1,18 @@
+// var y = 0;
+var x = 1;
+
+/*
+
+var x = 2;
+var z = 3;
+
+*/
+
+var desconto = 0.2;
+
+
+const PRECO = 2;
+// PRECO = 3 -> Constantes não podem ter seus valores alterados uma vez definidas, logo isso não irá funcionar
+
+var total = PRECO - desconto;
+
